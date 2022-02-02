fa1668485604ecd1b001522c365a4168761cf365
henrow <peterhenryowen@gmail.com>
2022-02-02 16:40:16 -0600

    changed to button interface

--- main.js
+++ main.js
@@ -1,64 +1,83 @@
 const playSet = ['rock', 'paper', 'scissors'];
+const buttons = document.querySelectorAll('button');
 
-function game(rounds) {
-    let playerWins = 0;
-    let computerWins = 0;
+buttons.forEach((button) => {
+    button.addEventListener('click', playRound);
+});
 
-    for (let round = 1; round <= rounds; round++) {
-        function playRound() {
-            const computerSelection = computerPlay(); 
-            const playerSelection = playerPlay();
+let playerWins = 0;
+let computerWins = 0;
+let round = 1;
 
-            console.log(`Round ${round}!`);
-            const result = checkWinner();
-            displayResult();
-            
-            function computerPlay () {
-                return playSet[Math.floor(Math.random()*playSet.length)];
-            }
+const playerScore = document.querySelector('#playerScore');
+const computerScore = document.querySelector('#computerScore');
 
-            function playerPlay(promptText = 'Your move: ') {
-                let playerChoice = prompt(`Round ${round}! ${promptText}`).toLowerCase();
-                if (!playSet.includes(playerChoice)) {
-                    playerChoice = playerPlay('Try again: ');
-                }
-                return playerChoice;
-            }
+const results = document.querySelector('#results');
 
-            function checkWinner() {
-                let winner = '';
-                if ( playerSelection === computerSelection ) {
-                    winner = 'draw';
-                } else if (
-                    (playerSelection === 'rock' && computerSelection === 'scissors')
-                    || (playerSelection === 'scissors' && computerSelection === 'paper')
-                    || (playerSelection === 'paper' && computerSelection === 'rock')         ) { 
-                    winner = 'player';
-                    playerWins += 1;
-                } else { 
-                    winner = 'computer'; 
-                    computerWins += 1;
-                }
-                return winner;
-            }
+function playRound(event) {
+    const computerSelection = computerPlay(); 
+    const playerSelection = this.id; 
 
-            function displayResult() {
-                if (result === 'draw') { console.log(`It's a draw - you both chose ${playerSelection}.`); }
-                else if ( result === 'player' ) { console.log(`Player wins - ${playerSelection} beats ${computerSelection}.`); }
-                else if ( result === 'computer' ) { console.log(`Computer wins - ${computerSelection} beats ${playerSelection}.`); }
-            }
+    console.log(`Round ${round}!`);
+    const result = checkWinner();
+    displayResult();
+    round++; 
+
+
+    if (computerWins >= 5) {
+        alert(`Computer wins with ${computerWins} to ${playerWins} games`);
+        resetGame();
+    } else if ( playerWins >= 5) {
+        alert(`Player wins with ${playerWins} to ${computerWins}`);
+        resetGame();
+    }
+    function computerPlay () {
+        return playSet[Math.floor(Math.random()*playSet.length)];
+    }
+
+    function checkWinner() {
+        let winner = '';
+        if ( playerSelection === computerSelection ) {
+            winner = 'draw';
+        } else if (
+            (playerSelection === 'rock' 
+                && computerSelection === 'scissors')
+            || (playerSelection === 'scissors' 
+                && computerSelection === 'paper')
+            || (playerSelection === 'paper' 
+                && computerSelection === 'rock')         ) { 
+            winner = 'player';
+            playerWins += 1;
+        } else { 
+            winner = 'computer'; 
+            computerWins += 1;
         }
-        playRound(round);
+        return winner;
     }
-    if ( playerWins > computerWins) {
-        console.log(`Player wins the series! ${playerWins} to ${computerWins}.`);
-    } else if ( computerWins > playerWins ) {
-        console.log(`Computer wins the series. ${computerWins} to ${playerWins}.`);
-    } else {
-        console.log(`DRAW with ${playerWins} apiece.`);
+
+    function displayResult() {
+        computerScore.textContent = computerWins;
+        playerScore.textContent = playerWins;
+
+        if (result === 'draw') { 
+            results.textContent = `It's a draw - you both chose ${playerSelection}.`; 
+        } else if ( result === 'player' ) { 
+            results.textContent = `Player wins - ${playerSelection} 
+                    beats ${computerSelection}.`; 
+        } else if ( result === 'computer' ) { 
+            results.textContent = `Computer wins - ${computerSelection} 
+                    beats ${playerSelection}.`; 
+        }
     }
+
+    function resetGame() {
+        computerWins = 0;
+        playerWins = 0;
+        round = 1;
+        results.textContent = ' ';
+        computerScore.textContent = computerWins;
+        playerScore.textContent = playerWins;
+    }
+
 }
-
-game(3);
-
                 
